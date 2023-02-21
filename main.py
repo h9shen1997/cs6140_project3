366b42916ea83960e89c7111d6c3d798658298cc
Haotian Shen <shenhtgxyz@gmail.com>
2023-02-21 01:21:28 -0800

    add multiple ML models

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from _decimal import Decimal, getcontext
+from time import time
 from typing import Tuple, Dict, List
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import tensorflow as tf
 from IPython.display import display
+from _decimal import Decimal, getcontext
+from keras.layers import Dense
+from keras.models import Sequential
 from pandas import DataFrame
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression, SGDClassifier, RidgeClassifier, Perceptron
+from sklearn.metrics import (accuracy_score, confusion_matrix, classification_report,
+                             ConfusionMatrixDisplay)
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 # set the display of DataFrame to display full rows and columns.
 pd.set_option('display.max_rows', None)
@@ -40,7 +50,7 @@ def flip_bits(num: int, length: int) -> List:
     return result
 
 
-def category_to_binary(df: DataFrame) -> Dict:
+def one_hot_encoding(df: DataFrame) -> Dict:
     """Transforms the categorical data using one-hot encoding.
 
     The categorical value will be transformed into an integer representation of its binary encoding.
@@ -74,7 +84,7 @@ def clean_data(df: DataFrame) -> DataFrame:
     :return: a cleaned version of pandas DataFrame.
     """
     features = df.columns
-    category_dict = category_to_binary(df)
+    category_dict = one_hot_encoding(df)
     num_rows = len(df)
     num_cols = len(df.columns)
     cleaned_data = []
@@ -189,7 +199,67 @@ def predict(train_X, train_y, test_X, test_y, model) -> float:
     return accuracy
 
 
+def train_eval_model(c, X_train, y_train, X_test, y_test, CLF):
+    def pred():
+        t0 = time()
+        p_train, p_test = model.predict(X_train), model.predict(X_test)
+        t = time() - t0
+        return t, p_train, p_test
+
+    def eval_model():
+        acc_train = accuracy_score(y_train, p_train)
+        print(f'Training Accuracy={acc_train:4.3f}')
+        print(classification_report(y_train, p_train))
+
+        acc_test = accuracy_score(y_test, p_test)
+        print(f'Test Accuracy={acc_test:4.3f}')
+        print(classification_report(y_test, p_test))
+
+        expected_pred = model.score(X_test, y_test)
+        pred_variance = expected_pred * (1 - expected_pred)
+        mse = ((y_test - p_test) ** 2).mean()
+        bias = mse - pred_variance
+        print(f'Bias: {bias:.2f}')
+        print(f'Variance: {pred_variance:.2f}')
+
+    model_name = c
+    print(model_name)
+    print(X_train.shape)
+    print(y_train.shape)
+    model = CLF[c]
+
+    t0 = time()
+    model.fit(X_train, y_train)
+    time_train = time() - t0
+    print(f'Training time = {time_train:4.2f}')
+
+    # trained model
+    CLF[c] = model
+    t, p_train, p_test = pred()
+    print(f'Validation and Test time = {t:4.2f}')
+    eval_model()
+    print('Generating Confusion Matrix')
+    cnf_matrix = confusion_matrix(y_test, p_test)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cnf_matrix)
+    disp.plot()
+    plt.title(f'Confusion Matrix using {model_name}')
+    plt.show()
+
+
+def train_neural_network(X_train, y_train, X_test, y_test, dim):
+    model = Sequential()
+    model.add(Dense(10, input_dim=dim, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X_train, y_train, epochs=100, verbose=1, batch_size=32)
+
+    _, acc = model.evaluate(X_test, y_test)
+    print(f'ANN Accuracy: {acc * 100}%')
+
+
 def main():
+    np.random.seed(0)
+
     # import training and test data into DataFrame
     train_df = read_csv('data/heart_train_718.csv')
     test_df = read_csv('data/heart_test_200.csv')
@@ -201,8 +271,7 @@ def main():
     display(combined_df.head())
 
     # separate the cleaned data into X and y
-    X, y = combined_df.iloc[:, :-1], combined_df.iloc[:, -1]
-
+    X, y = combined_df.drop('HeartDisease', axis=1), combined_df['HeartDisease']
     # normalize the data
     X_normalized = normalize_data(X)
     X_train = X_normalized.iloc[:len(train_df), :]
@@ -210,6 +279,23 @@ def main():
     y_train = y.iloc[:len(train_df)]
     y_test = y.iloc[len(train_df):]
     column_names = X_train.columns
+
+    # feature selection
+    rfc = RandomForestClassifier(random_state=0)
+    rfc.fit(X, y)
+    importances = rfc.feature_importances_
+
+    feature_importances = {}
+    for i in range(len(importances)):
+        feature_importances[X.columns[i]] = {'importance': importances[i], 'index': i}
+
+    sorted_feature_importances = sorted(feature_importances.items(), key=lambda x: x[1]['importance'], reverse=True)
+    # Print the feature ranking
+    print("Feature ranking:")
+    for f in range(len(sorted_feature_importances)):
+        print(
+            f"{f + 1}. {sorted_feature_importances[f][0]} (index: {sorted_feature_importances[f][1]['index']}, "
+            f"importance: {sorted_feature_importances[f][1]['importance']})")
 
     # visualize the normalized training data using boxplot.
     # significant signals in the independent variables.
@@ -239,27 +325,42 @@ def main():
     # observe natural clustering within the training data by projecting it to 2-D
     observe_clustering(X_train)
 
-    # LogisticRegression model
-    LR = LogisticRegression(max_iter=1000, C=5, random_state=42)
+    CLF = {}
+    CLF['Logistic Regression'] = LogisticRegression(max_iter=1000, C=1.6, random_state=0)
+    CLF['SGDCClassifier L2'] = SGDClassifier(max_iter=1000, alpha=1e-5, penalty='l2', random_state=0)
+    CLF['SGDCClassifier L1'] = SGDClassifier(max_iter=1000, alpha=1e-5, penalty='l1', random_state=0)
+    CLF['Ridge Classifier'] = RidgeClassifier(max_iter=1000, tol=1e-2, solver='sag', random_state=0)
+    CLF['Perceptron'] = Perceptron(max_iter=1000, random_state=0)
+    CLF['BernoulliNB'] = BernoulliNB(alpha=.01)
+    CLF['K-Nearest Neighbor'] = KNeighborsClassifier(n_neighbors=5, weights='distance', p=2)
+    CLF['Support Vector Machine'] = SVC(max_iter=1000, C=1.0, tol=1e-3, kernel='rbf', gamma='scale', random_state=0)
+    CLF['Decision Tree Classifier'] = DecisionTreeClassifier(random_state=0)
+    CLF['Random Forest Classifier'] = RandomForestClassifier(random_state=0)
+    print('\nAvailable classifiers:')
+    for c in CLF:
+        print('- ', c)
 
-    # predict the accuracy of performance using LogisticRegression model on the test set.
-    predict(X_train, y_train, X_test, y_test, LR)
-
-    # use the selected salient features to predict accuracy again to determine if they
-    # can almost achieve the same result
+    # use the selected salient features to train different models
     X_train_selected = X_train.loc[:, [column_names[i] for i in [0, 2, 3, 4, 7, 8, 9, 10]]]
     X_test_selected = X_test.loc[:, [column_names[i] for i in [0, 2, 3, 4, 7, 8, 9, 10]]]
-    predict(X_train_selected, y_train, X_test_selected, y_test, LR)
+    for c in CLF:
+        train_eval_model(c, X_train_selected, y_train, X_test_selected, y_test, CLF)
+        print('_' * 80)
+        print('_' * 80)
+
+    # use neural network
+    tf.random.set_seed(0)
+    train_neural_network(X_train_selected, y_train, X_test_selected, y_test, 8)
 
     # feature engineering for max heart rate.
     # heart rate may not have linear relationship, compute quadratic and cubed terms for it
-    print('feature engineering quadratic and cubed term for max heart rate')
-    X_train_selected['MaxHR_squared'] = X_train_selected['MaxHR'].apply(lambda x: x ** 2)
-    X_test_selected['MaxHR_squared'] = X_test_selected['MaxHR'].apply(lambda x: x ** 2)
-    X_train_selected['MaxHR_cubed'] = X_train_selected['MaxHR'].apply(lambda x: x ** 3)
-    X_test_selected['MaxHR_cubed'] = X_test_selected['MaxHR'].apply(lambda x: x ** 3)
-    display(X_train_selected.head())
-    predict(X_train_selected, y_train, X_test_selected, y_test, LR)
+    # print('feature engineering quadratic and cubed term for max heart rate')
+    # X_train_selected['MaxHR_squared'] = X_train_selected['MaxHR'].apply(lambda x: x ** 2)
+    # X_test_selected['MaxHR_squared'] = X_test_selected['MaxHR'].apply(lambda x: x ** 2)
+    # X_train_selected['MaxHR_cubed'] = X_train_selected['MaxHR'].apply(lambda x: x ** 3)
+    # X_test_selected['MaxHR_cubed'] = X_test_selected['MaxHR'].apply(lambda x: x ** 3)
+    #
+    # train_ANN(X_train_selected, y_train, X_test_selected, y_test, 10)
 
 
 if __name__ == '__main__':
